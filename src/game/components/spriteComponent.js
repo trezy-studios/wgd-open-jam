075d8c002d47fa09cbb1083345a81383fff1a719
Trezy <t@trezy.com>
2023-04-03 15:43:07 -0500

    stop sprite before switching to next animation

--- src/game/components/spriteComponent.js
+++ src/game/components/spriteComponent.js
@@ -64,6 +64,7 @@ export function spriteComponent(spritesheetName, defaultAnimationName) {
 		sprite.play()
 
 		if (state.sprite) {
+			state.sprite.gotoAndStop(0)
 			pixiApp.stage.removeChild(state.sprite)
 		}
 
