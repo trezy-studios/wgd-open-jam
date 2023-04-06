292ad9e14d14af2b3c201d516ba8a7e4a06f7fe1
Trezy <t@trezy.com>
2023-04-05 23:57:31 -0500

    comment out the camera system for now

--- src/game/systems/cameraSystem.js
+++ src/game/systems/cameraSystem.js
@@ -1,7 +1,7 @@
 // Local imports
-import { getCameras } from '../entities/selectors/getCameras.js'
-import { getPlayers } from '../entities/selectors/getPlayers.js'
-import { store } from '../../store/store.js'
+// import { getCameras } from '../entities/selectors/getCameras.js'
+// import { getPlayers } from '../entities/selectors/getPlayers.js'
+// import { store } from '../../store/store.js'
 
 
 
@@ -9,53 +9,51 @@ import { store } from '../../store/store.js'
 
 /** Updates the camera's position based on the player character's position. */
 export function cameraSystem() {
-	const { canvas } = store.state
+	// const { canvas } = store.state
 
-	return
+	// for (const camera of getCameras()) {
+	// 	camera.size.height = canvas.height
+	// 	camera.size.width = canvas.width
 
-	for (const camera of getCameras()) {
-		camera.size.height = canvas.height
-		camera.size.width = canvas.width
+	// 	const focusArea = {
+	// 		height: null,
+	// 		width: null,
+	// 		x: null,
+	// 		y: null,
+	// 	}
 
-		const focusArea = {
-			height: null,
-			width: null,
-			x: null,
-			y: null,
-		}
+	// 	for (const player of getPlayers()) {
+	// 		if (!focusArea.x) {
+	// 			focusArea.x = player.position.x
+	// 		} else {
+	// 			focusArea.x = Math.min(focusArea.x, player.position.x)
+	// 		}
 
-		for (const player of getPlayers()) {
-			if (!focusArea.x) {
-				focusArea.x = player.position.x
-			} else {
-				focusArea.x = Math.min(focusArea.x, player.position.x)
-			}
+	// 		if (!focusArea.y) {
+	// 			focusArea.y = player.position.y
+	// 		} else {
+	// 			focusArea.y = Math.min(focusArea.y, player.position.y)
+	// 		}
 
-			if (!focusArea.y) {
-				focusArea.y = player.position.y
-			} else {
-				focusArea.y = Math.min(focusArea.y, player.position.y)
-			}
+	// 		if (!focusArea.width) {
+	// 			focusArea.width = player.size.width
+	// 		} else {
+	// 			focusArea.width = Math.max(focusArea.width, focusArea.x + player.size.width)
+	// 		}
 
-			if (!focusArea.width) {
-				focusArea.width = player.size.width
-			} else {
-				focusArea.width = Math.max(focusArea.width, focusArea.x + player.size.width)
-			}
+	// 		if (!focusArea.height) {
+	// 			focusArea.height = player.size.height
+	// 		} else {
+	// 			focusArea.height = Math.max(focusArea.height, focusArea.y + player.size.height)
+	// 		}
+	// 	}
 
-			if (!focusArea.height) {
-				focusArea.height = player.size.height
-			} else {
-				focusArea.height = Math.max(focusArea.height, focusArea.y + player.size.height)
-			}
-		}
+	// 	const centerPoint = {
+	// 		x: focusArea.x + (focusArea.width / 2),
+	// 		y: focusArea.y + (focusArea.height / 2),
+	// 	}
 
-		const centerPoint = {
-			x: focusArea.x + (focusArea.width / 2),
-			y: focusArea.y + (focusArea.height / 2),
-		}
-
-		camera.position.x = (camera.size.width / 2) - (centerPoint.x * 2)
-		camera.position.y = (camera.size.height / 2) - (centerPoint.y * 2)
-	}
+	// 	camera.position.x = (camera.size.width / 2) - (centerPoint.x * 2)
+	// 	camera.position.y = (camera.size.height / 2) - (centerPoint.y * 2)
+	// }
 }
