c86adb3c67f0664d82e63bce33e9a97746eaf5db
Trezy <t@trezy.com>
2023-08-03 16:30:04 -0500

    add method for adding an item to the inventory

--- src/game/components/inventoryComponent.js
+++ src/game/components/inventoryComponent.js
@@ -7,15 +7,48 @@
  * @returns {object} The entity's initial inventory.
  */
 export function inventoryComponent(config = {}) {
-	const {
-		capacity = 10,
-		contents = [],
-	} = config
-
-	return {
+	const result = {
 		inventory: {
-			capacity,
-			contents,
+			capacity: config.capacity ?? 8,
+			contents: config.contents,
+
+			/**
+			 * Adds an item to this inventory.
+			 *
+			 * @param {object} item The item to be added.
+			 * @param {number} [slot] The slot to place the item in. If not set, item will be placed in the next empty slot.
+			 * @returns {boolean} Whether the item was added successfully.
+			 */
+			addItem(item, slot = 0) {
+				const {
+					capacity,
+					contents,
+				} = this
+
+				let slotToCheck = slot
+				let success = false
+
+				while (slotToCheck < capacity) {
+					if (contents[slotToCheck] === null) {
+						this.contents = [...this.contents]
+						this.contents[slotToCheck] = item
+						success = true
+						break
+					}
+
+					slotToCheck += 1
+				}
+
+				return success
+			},
 		},
 	}
+
+	let slotIndex = 0
+	while (slotIndex < result.inventory.capacity) {
+		result.inventory.contents[slotIndex] = config.contents[slotIndex] ?? null
+		slotIndex += 1
+	}
+
+	return result
 }
